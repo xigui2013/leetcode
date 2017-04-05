5ab406c031fb792cdab2d7d47153abf809fe5c29
jianwenwei1 <jianwenwei1@creditease.cn>
2017-04-06 00:19:03 +0800

    init

---
+++ src/neu/KeyboardRow.java
@@ -0,0 +1,49 @@
+package neu;
+
+import java.util.ArrayList;
+
+public class KeyboardRow {
+
+    public static void main(String[] args) {
+        String[] words = { "Hello", "Alaska", "Dad", "Peace" };
+        for (String word : findWords(words)) {
+            System.out.println(word);
+        }
+        // System.out.println();
+    }
+
+    public static String[] findWords(String[] words) {
+        ArrayList<String> result = new ArrayList<String>();
+        String row1 = "qwertyuiop";
+        String row2 = "asdfghjkl";
+        String row3 = "zxcvbnm";
+        for (String word : words) {
+            int atRow = -1;
+            boolean isOneRow = true;
+            for(char c :word.toLowerCase().toCharArray()){
+                int currentRow = -1;
+                if(row1.indexOf(c) != -1){
+                    currentRow = 1;
+                }
+                if(row2.indexOf(c) != -1){
+                    currentRow = 2;
+                }
+                if(row3.indexOf(c) != -1){
+                    currentRow = 3;
+                }
+                if(atRow == -1){
+                    atRow = currentRow;
+                    continue;
+                }
+                if(atRow != currentRow){
+                    isOneRow = false;
+                    break;
+                }
+            }
+            if(isOneRow){
+                result.add(word);
+            }
+        }
+        return result.toArray(new String[result.size()]);
+    }
+}
