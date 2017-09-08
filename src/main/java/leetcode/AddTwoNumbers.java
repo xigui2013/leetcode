6a3fd228bb81235ea6162527b6f241cc5ffd38b0
魏建文 <jianwenwei1@creditease.cn>
2017-09-08 09:03:37 +0800

    20170818 Add Two Numbers

--- src/main/java/leetcode/AddTwoNumbers.java
+++ src/main/java/leetcode/AddTwoNumbers.java
@@ -29,8 +29,8 @@ public class AddTwoNumbers {
             result.next = new ListNode(sum % 10);
             result = result.next;
 
-            l1 = l1 == null?l1:l1.next;
-            l2 = l2 == null?l2:l2.next;
+            l1 = l1 == null? null :l1.next;
+            l2 = l2 == null? null :l2.next;
         }
         return header.next;
     }
